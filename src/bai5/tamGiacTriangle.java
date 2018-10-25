61d005411ab4a23e467243e0a709d6239bd4f384
ThanhcongPhung <43209065+ThanhcongPhung@users.noreply.github.com>
2018-10-25 13:57:21 +0700

    Update tamGiacTriangle.java

--- src/bai5/tamGiacTriangle.java
+++ src/bai5/tamGiacTriangle.java
@@ -1,41 +1,49 @@
 package bai5;
+
 import java.util.Scanner;
+
 public class tamGiacTriangle {
-    int a,b,c;
-    tamGiacTriangle(){}
-    tamGiacTriangle(int a,int b,int c){
-        this.a=a;
-        this.b=b;
-        this.c=c;
+    private int a, b, c;
+
+    tamGiacTriangle() {
     }
-    public void inputInfo(){
+
+    tamGiacTriangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void inputInfo() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap A: ");
-        a=sc.nextInt();
+        a = sc.nextInt();
         System.out.println("Nhap B: ");
-        b=sc.nextInt();
+        b = sc.nextInt();
         System.out.println("Nhap C: ");
-        c=sc.nextInt();
+        c = sc.nextInt();
     }
-    public void kieuTG(){
-        int a=0,b=0,c=0;
 
-        if((a==c)||(a==b)){
-            System.out.println("Tam giac can");
-        }else if((a==c)&&(a==b)){
+    public void kieuTG() {
+
+
+        if ((a == c) && (a == b)) {
             System.out.println("Tam giac deu");
-        }else if(((a^2)==(b^2+c^2))||((b^2)==(a^2+c^2))||((c^2)==(b^2+c^2))){
+        } else if ((a == c) || (a == b)) {
+            System.out.println("Tam giac can");
+        } else if (((a ^ 2) == (b ^ 2 + c ^ 2)) || ((b ^ 2) == (a ^ 2 + c ^ 2)) || ((c ^ 2) == (b ^ 2 + c ^ 2))) {
             System.out.println("Tam giac vuong");
-        }else if(!((a==c)&&(a==b))){
+        } else if ((a + b) > c || (a + c) > b || (b + c) > a) {
             System.out.println("Tam giac thuong");
         }
     }
-    public void tinhToan(){
+
+    public void tinhToan() {
         float p;
-        p=a+b+c;
+        p = a + b + c;
         System.out.println("Chu vi tam giac la: " + p);
         float s;
-        s=(float)Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        s = (float) Math.sqrt(p * (p - a) * (p - b) * (p - c));
         System.out.println("Dien tich tam giac la: " + s);
     }
 
