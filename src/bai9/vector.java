0dab7172610d296e185a0765bfab8a63004498a5
ThanhcongPhung <43209065+ThanhcongPhung@users.noreply.github.com>
2018-10-25 13:55:55 +0700

    Update vector.java

--- src/bai9/vector.java
+++ src/bai9/vector.java
@@ -1,40 +1,101 @@
-package bai9;
-import java.util.Scanner;
+package bai10;
 
-public class vector {
-    private int hddau,hdcuoi,tddau,tdcuoi;
-    public vector(){}
-    public vector(int x1,int x2,int y1,int y2){
-        this.hddau=x1;
-        this.tddau=y1;
-        this.hdcuoi=x2;
-        this.tdcuoi=y2;
+
+public class DaThuc {
+    private int[] heSo;
+    private int bac;
+
+    public DaThuc(int a, int b) {
+        heSo = new int[b + 1];
+        heSo[b] = a;
+        bac = degree();
     }
-    public int nhapX(){
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        return x;
+
+    public DaThuc(DaThuc p) {
+        heSo = new int[p.heSo.length];
+        for (int i = 0; i < p.heSo.length; i++) {
+            heSo[i] = p.heSo[i];
+        }
+        bac = p.degree();
     }
-    public vector inputInfo(){
-        System.out.print("Phan hoanh do dau = ");
-        int x1=nhapX();
-        System.out.print("Phan tung do dau = ");
-        int y1=nhapX();
-        System.out.print("Phan hoanh do cuoi = ");
-        int x2=nhapX();
-        System.out.print("Phan tung do cuoi = ");
-        int y2=nhapX();
-        vector A= new vector(x1,y1,x2,y2);
-        return A;
+
+    public int degree() {
+        int d = 0;
+        for (int i = 0; i < heSo.length; i++) {
+            if (heSo[i] != 0) d = i;
+        }
+        return d;
+    }
+
+    public DaThuc cong(DaThuc b) {
+        DaThuc a = this;
+        DaThuc c = new DaThuc(0, Math.max(a.bac, b.bac));
+        for (int i = 0; i <= a.bac; i++) c.heSo[i] += a.heSo[i];
+        for (int i = 0; i <= b.bac; i++) c.heSo[i] += b.heSo[i];
+        c.bac = c.degree();
+        return c;
+    }
+
+    public DaThuc nhan(DaThuc b) {
+        DaThuc a = this;
+        DaThuc c = new DaThuc(0, a.bac + b.bac);
+        for (int i = 0; i <= a.bac; i++)
+            for (int j = 0; j <= b.bac; j++)
+                c.heSo[i + j] += (a.heSo[i] * b.heSo[j]);
+        c.bac = c.degree();
+        return c;
+    }
+
+    public int coeff() {
+        return coeff(degree());
+    }
+
+
+    // get the coefficient for degree d
+    public int coeff(int degree) {
+        if (degree > this.degree()) throw new RuntimeException("bad degree");
+        return heSo[degree];
+    }
+
+    public String toString() {
+        if (bac == 0) return "" + heSo[0];
+        if (bac == 1) return heSo[1] + "x + " + heSo[0];
+        String s = heSo[bac] + "x^" + bac;
+        for (int i = bac - 1; i >= 0; i--) {
+            if (heSo[i] == 0) {
+                continue;
+            } else if (heSo[i] > 0) {
+                s = s + " + " + (heSo[i]);
+            } else if (heSo[i] < 0) s = s + " - " + (-heSo[i]);
+            if (i == 1) {
+                s = s + "x";
+            } else if (i > 1) s = s + "x^" + i;
+        }
+        return s;
     }
 
     public static void main(String[] args) {
-        System.out.println("Nhap vector A: ");
-        vector A = new vector();
-        A = A.inputInfo();
-        System.out.println("Nhap vector B: ");
-        vector B=new vector();
-        B=B.inputInfo();
+        DaThuc zero = new DaThuc(0, 0);
+
+        DaThuc p1 = new DaThuc(1, 3);
+        DaThuc p2 = new DaThuc(2, 2);
+        DaThuc p3 = new DaThuc(4, 0);
+        DaThuc p4 = new DaThuc(0, 1);
+        DaThuc p = p1.cong(p2).cong(p3).cong(p4);   // 4x^3 + 3x^2 + 1
+
+        DaThuc q1 = new DaThuc(1, 1);
+        DaThuc q2 = new DaThuc(3, 0);
+        DaThuc q = q1.cong(q2);                     // 3x^2 + 5
+
+
+        DaThuc r = p.cong(q);
+        DaThuc s = p.nhan(q);
+
+        System.out.println("zero(x) =     " + zero);
+        System.out.println("p(x) =        " + p);
+        System.out.println("q(x) =        " + q);
+        System.out.println("p(x) + q(x) = " + r);
+        System.out.println("p(x) * q(x) = " + s);
 
 
     }
